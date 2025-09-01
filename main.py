d93caaac38379cecfa0906e73524d2c48dd0abb2
Stephanie Lee <stephl25@uci.edu>
2025-09-01 14:57:46 -0700

    create_board makes board of buttons

--- main.py
+++ main.py
@@ -18,20 +18,29 @@ def new_game():
     pass
 
 def reset_button():
-    reset_button = Button(text="Restart", font=('Verdana', 20), command=new_game)
+    reset_button = Button(text="Restart", font=('Verdana', 15), command=new_game)
     reset_button.pack(side="top")
 
+def create_board(window : Tk):
+    frame = Frame(window)
+    frame.pack()
+    for row in range(3):
+        for col in range(3):
+            board[row][col] = Button(frame, text="", font=('Verdana', 40), width=5, height=2,
+                                     command=lambda row=row, col=col : next_turn(row, col))
+            board[row][col].grid(row=row, column=col)
 
-def main():
+
+def main(window : Tk):
     player = choice(players)
-    label = Label(text=f"{player} turn", font=('Verdana', 40))
+    label = Label(text=f"{player} turn", font=('Verdana', 30))
     label.pack(side="top")
     reset_button()
+    create_board(window)
 
 
 if __name__ == "__main__":
     window = Tk()
     window.title("TicTacToe")
-    window.geometry("480x720")
-    main()
+    main(window)
     window.mainloop()
