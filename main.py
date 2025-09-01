d454f0e53492ff7ca07a2757e546164a74a1bccb
Stephanie Lee <stephl25@uci.edu>
2025-09-01 14:42:04 -0700

    turn label and reset button

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from random import choices
+from random import choice
+from assests import *
 
 
 def next_turn():
@@ -16,9 +17,21 @@ def empty_spaces():
 def new_game():
     pass
 
+def reset_button():
+    reset_button = Button(text="Restart", font=('Verdana', 20), command=new_game)
+    reset_button.pack(side="top")
+
+
+def main():
+    player = choice(players)
+    label = Label(text=f"{player} turn", font=('Verdana', 40))
+    label.pack(side="top")
+    reset_button()
+
 
 if __name__ == "__main__":
     window = Tk()
     window.title("TicTacToe")
     window.geometry("480x720")
+    main()
     window.mainloop()
