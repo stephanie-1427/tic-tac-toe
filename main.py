de880f124bd12394b7ade25cbd87563304828572
Stephanie Lee <stephl25@uci.edu>
2025-09-01 14:17:10 -0700

    functions created

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from tkinter import *
 from random import choices
 
 
+def next_turn():
+    pass
+
+def check_win():
+    # a win
+    pass
+
+def empty_spaces():
+    # recognize a tie
+    pass
+
+def new_game():
+    pass
+
 
 if __name__ == "__main__":
     window = Tk()
